29194ade93d9a10f807dde68c04832dde45fd22b
erenst <erenst2002@hotmail.com>
2022-10-29 12:09:15 +1030

    tiny change some code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,7 +9,6 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        // ...add more providers here
     ],
     pages: {
         signIn: "/auth/signin"
